c884c0730622798e2b3abf517b0c68c90654872b
J. Klint <85409085+j-klint@users.noreply.github.com>
2024-06-18 12:17:30 +0200

    Decrease student name input length limit
    because datatype was changed from varchar(800) to varchar(100) in the
    database script.

--- Harjoittelu/Models/Student.cs
+++ Harjoittelu/Models/Student.cs
@@ -17,7 +17,7 @@ namespace Harjoittelu.Models
         [Key] public int Id { get; set; }
 
         [Required]
-        [MaxLength(400)]
+        [MaxLength(50)]
         public string Name { get; set; }
 
         [Required]
